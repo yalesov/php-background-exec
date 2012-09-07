e4b9664aa1aee4ae21d1a32741c368d3a3677dfb
heartsentwined <heartsentwined@cogito-lab.com>
2012-09-08 03:46:08 +0800

    first release

---
+++ README.md
@@ -0,0 +1,22 @@
+# Heartsentwined\BackgroundExec
+
+Close a client HTTP connection and continue script execution in background.
+
+# Installation
+
+[Composer](http://getcomposer.org/):
+
+```json
+{
+    "require": {
+        "heartsentwined/background-exec": "1.*"
+    }
+}
+```
+
+# Usage
+
+```php
+use Heartsentwined\BackgroundExec\BackgroundExec;
+BackgroundExec::start();
+```
---
+++ src/Heartsentwined/BackgroundExec/BackgroundExec.php
@@ -0,0 +1,35 @@
+<?php
+namespace Heartsentwined\BackgroundExec;
+
+class BackgroundExec
+{
+    /**
+     * close a client HTTP connection
+     * and start (continue) script execution in background
+     *
+     * @return void
+     */
+    public static function start()
+    {
+        try {
+            ob_end_clean();
+            header('Connection: close;');
+            header('Content-Encoding: none;');
+            ignore_user_abort(true); // optional
+            ob_start();
+            //echo ('Text user will see');
+            $size = ob_get_length();
+            header("Content-Length: $size;");
+            ob_end_flush();     // Strange behaviour, will not work
+            flush();            // Unless both are called !
+            ob_end_clean();
+
+            //do processing here
+            //sleep(5);
+
+            //echo('Text user will never see');
+            //do some processing
+        } catch (\Exception $e) {
+        }
+    }
+}
