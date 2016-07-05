4c9782d87cb78d14947f9ed10c0c8635177cc735
Yulij Andreevich Lesov <yalesov@gmail.com>
2016-07-05 21:38:51 +0800

    switch from 4 spaces to 2 spaces

--- src/Yalesov/BackgroundExec/BackgroundExec.php
+++ src/Yalesov/BackgroundExec/BackgroundExec.php
@@ -3,28 +3,28 @@ namespace Yalesov\BackgroundExec;
 
 class BackgroundExec
 {
-    /**
-     * close a client HTTP connection
-     * and start (continue) script execution in background
-     *
-     * @return void
-     */
-    public static function start()
-    {
-        try {
-            ob_end_clean();
-            header('Connection: close;');
-            header('Content-Encoding: none;');
-            ignore_user_abort(true); // optional
-            ob_start();
-            //echo ('Text user will see');
-            $size = ob_get_length();
-            header("Content-Length: $size;");
-            ob_end_flush();     // Strange behaviour, will not work
-            flush();            // Unless both are called !
-            ob_end_clean();
-            //echo('Text user will never see');
-        } catch (\Exception $e) {
-        }
+  /**
+   * close a client HTTP connection
+   * and start (continue) script execution in background
+   *
+   * @return void
+   */
+  public static function start()
+  {
+    try {
+      ob_end_clean();
+      header('Connection: close;');
+      header('Content-Encoding: none;');
+      ignore_user_abort(true); // optional
+      ob_start();
+      //echo ('Text user will see');
+      $size = ob_get_length();
+      header("Content-Length: $size;");
+      ob_end_flush();   // Strange behaviour, will not work
+      flush();      // Unless both are called !
+      ob_end_clean();
+      //echo('Text user will never see');
+    } catch (\Exception $e) {
     }
+  }
 }
